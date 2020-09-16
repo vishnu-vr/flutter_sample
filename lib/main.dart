ea6731baf5a1695b7918976bb773ac9551b100da
vishnuramesh <vishnuramesh19@gmail.com>
2020-09-16 21:00:01 +0530

    atm layout v1 complete

--- lib/main.dart
+++ lib/main.dart
@@ -31,9 +31,9 @@ class _HomeState extends State<Home> {
         padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 10.0),
         child: ListView(
           children: [
-            AtmCard("Axis Bank", "0000-0000-0000-0000"),
-            AtmCard("Federal Bank", "0000-0000-0000-0000"),
-            AtmCard("State Bank", "0000-0000-0000-0000"),
+            AtmCard("Axis Bank", "0000-0000-0000-0000", "08/22", "220"),
+            AtmCard("Federal Bank", "0000-0000-0000-0000", "08/22", "220"),
+            AtmCard("State Bank", "0000-0000-0000-0000", "08/22", "220"),
           ],
         ),
       ),
@@ -44,7 +44,9 @@ class _HomeState extends State<Home> {
 class AtmCard extends StatelessWidget {
   final String bankName;
   final String cardNumber;
-  AtmCard(this.bankName, this.cardNumber);
+  final String expDate;
+  final String cvv;
+  AtmCard(this.bankName, this.cardNumber, this.expDate, this.cvv);
 
   @override
   Widget build(BuildContext context) {
@@ -57,21 +59,57 @@ class AtmCard extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.all(20.0),
           child: Center(
-            child: Column(children: [
-              Text(
-                this.bankName,
-                style: TextStyle(
-                  letterSpacing: 1.0,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15.0,
-                  color: Colors.white,
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  this.bankName.toUpperCase(),
+                  style: TextStyle(
+                    letterSpacing: 1.0,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15.0,
+                    color: Colors.black87,
+                  ),
                 ),
-              ),
-              Divider(
-                color: Colors.black,
-              ),
+                Divider(color: Colors.black),
+                AtmCardInsideInfo("Card Number", this.cardNumber),
+                AtmCardInsideInfo("Exp Date", this.expDate),
+                AtmCardInsideInfo("CVV", this.cvv),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class AtmCardInsideInfo extends StatelessWidget {
+  final String holderName;
+  final String holderInfo;
+  AtmCardInsideInfo(this.holderName, this.holderInfo);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            this.holderName + " :",
+            style: TextStyle(
+              letterSpacing: 1.0,
+              fontWeight: FontWeight.bold,
+              fontSize: 18.0,
+              color: Colors.black,
+            ),
+          ),
+          // SizedBox(height: 1.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
               Text(
-                this.cardNumber,
+                this.holderInfo,
                 style: TextStyle(
                   letterSpacing: 1.0,
                   fontWeight: FontWeight.bold,
@@ -79,9 +117,13 @@ class AtmCard extends StatelessWidget {
                   color: Colors.black,
                 ),
               ),
-            ]),
+              FlatButton(
+                onPressed: () => {print("copy to clipboard")},
+                child: Icon(Icons.content_copy, size: 22.0),
+              )
+            ],
           ),
-        ),
+        ],
       ),
     );
   }
